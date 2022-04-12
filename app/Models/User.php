3fd8593225d76bb2e8cf94c5b255f78a6d69728a
Nguyen Thanh Luan <luann4099@gmail.com>
2022-04-12 16:18:06 +0700

    update code

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,6 @@ class User extends Authenticatable
 
     public function getFullAvatarPathAttribute()
     {
-        return asset('storage/avatar/' . $this->avatar);
+        return asset( $this->avatar);
     }
 }
