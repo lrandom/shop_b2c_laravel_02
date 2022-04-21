9b82347f17e2a9f2891ca9d824fc7f40b31ef570
Windows 11 <shivershady@gmail.com>
2022-04-21 04:02:34 -0600

    update

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ChatController extends Controller
 {
@@ -17,5 +18,12 @@ class ChatController extends Controller
         $chat->user_id = $userId;
         $chat->save();
         return response()->json(['success' => true, 'message' => 'Thêm thành công']);
+    }
+    public function getListMess($id)
+    {
+        $listMess = Message::orderBy('id','DESC')->skip(0)->take(10)->get()->reverse();
+        return response()->json([
+            'messages' => $listMess,
+        ]);
     }
 }
